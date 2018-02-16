0faaf03909f6f83730281d6c9e8578809dd29c66
Pawel <pjanic98@gmail.com>
2018-02-16 13:08:31 +0100

    heroku ready to open

--- target/generated-sources/annotations/com/github/pawelo98/product/ProductDTOMapperImpl.java
+++ target/generated-sources/annotations/com/github/pawelo98/product/ProductDTOMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2018-02-16T10:57:22+0100",
+    date = "2018-02-16T12:29:20+0100",
     comments = "version: 1.2.0.Final, compiler: javac, environment: Java 1.8.0_144 (Oracle Corporation)"
 )
 @Component
